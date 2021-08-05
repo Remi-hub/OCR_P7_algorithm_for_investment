ae0082409884e8b7fa4796401ef796255d516ae4
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-08-05 18:11:01 +0200

    renamed file & progress

--- optimized.py
+++ optimized.py
@@ -18,16 +18,28 @@ def all_possibilities(remaining_shares, remaining_budget):
         yield []
 
     else:
-        for share in remaining_shares:
+        for i, share in enumerate(remaining_shares):
             if float(share['price']) <= remaining_budget:
-                remaining_budget = remaining_budget - float(share['price'])
-                remaining_shares.remove(share)
-                for possibility in all_possibilities(remaining_shares, remaining_budget):
+                # remaining_budget = remaining_budget - float(share['price'])
+                updated_shares = remaining_shares[i:].copy()
+                updated_shares.remove(share)
+                for possibility in all_possibilities(updated_shares, remaining_budget - float(share['price'])):
                     yield possibility + [share]
             # if the price is greater than the budget we yield an empty list
             else:
                 yield []
 
 
-for result in all_possibilities(info, 500.0):
+max_profit = 0
+best_combinaison_of_share = []
+for result in all_possibilities(info, 20.0):
     print(result)
+    total_profit = sum([share['profit in euros'] for share in result])
+    if total_profit > max_profit:
+        max_profit = total_profit
+        best_combinaison_of_share = result
+        cost = sum([float(share['price']) for share in result])
+        # print(cost)
+
+print(best_combinaison_of_share)
+print(max_profit)
